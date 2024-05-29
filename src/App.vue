838050b678340c862d126f801c68878e4f342052
annguyen186305 <annguyen186305@gmail.com>
2024-05-29 16:35:50 +0700

    So sanh giua methods va computed va su dung v-model

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <computedProps> </computedProps>
+  </div>
+</template>
+
+<script>
+
+import computedProps from "@/components/computedProps.vue";
+
+export default {
+  name: 'App',
+  components: {
+    computedProps
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/computedProps.vue
@@ -0,0 +1,61 @@
+<template>
+<div>
+  <h1> FirstName = {{firstName}}</h1>
+  <input v-model="firstName" type="text" placeholder="Nhap firstName">
+  <br>
+  <p> {{message}}</p>
+  <p> {{reverseMessage}}</p>
+<!--  <p> {{reverseMessage()}}</p>-->
+  <br>
+  <button v-on:click="a++">A = A + 1 </button>
+  <button v-on:click="b++">B = B + 1 </button>
+  <p>Number + A = {{addA}}</p>
+  <p>Number + B = {{addB}}</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: "computedProps",
+  data () {
+    return {
+      message: 'Hello world',
+      a: 0,
+      b: 0,
+      number: 20,
+      firstName:''
+    }
+  },
+  methods: {
+    // reverseMessage: function () {
+    //   return this.message.split('').reverse().join('')
+    // },
+    // addA(){
+    //   console.log('addA run');
+    //   return this.a + this.number
+    // },
+    // addB(){
+    //   console.log('addB run');
+    //   return this.b + this.number
+    // },
+  },
+  computed: {
+    reverseMessage: function () {
+      return this.message.split('').reverse().join('')
+    },
+    addA(){
+      console.log('addA run');
+      return this.a + this.number
+    },
+    addB(){
+      console.log('addB run');
+      return this.b + this.number
+    },
+  },
+}
+
+</script>
+
+<style scoped>
+
+</style>
